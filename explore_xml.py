92f44c8e2341f77ebcaacfcdef89c046ac4a769b
Karbo-101 <kaiboettinger@web.de>
2024-02-23 21:19:42 +0100

    Skript erweitert

--- explore_xml.py
+++ explore_xml.py
@@ -1,11 +1,38 @@
 from xml.sax import make_parser, handler
-
+# https://www.python-kurs.eu/python_XML_SAX.php
 class BuecherHandler(handler.ContentHandler):
+
+    def __init__(self):
+        self.authors = set()
+        self.titles = set()
+        self.current_content = ""
+
     def startElement(self, name, attrs):
-        print(name)
+        self.current_content = ""
+
+    def characters(self, content):
+        self.current_content += content.strip()
+
+    def endElement(self, name):
+        if name == "title":
+            self.titles.add(self.current_content)
+        elif name == "author":
+            self.authors.add(self.current_content)
+
+    def getTitles(self):
+        return self.titles
+
+    def getAuthors(self):
+        return self.authors
 
 
 parser = make_parser()
 b = BuecherHandler()
 parser.setContentHandler(b)
 parser.parse("buecher.xml")
+
+print("Authors: ")
+print(b.getAuthors())
+
+print("Titles:")
+print(b.getTitles())
